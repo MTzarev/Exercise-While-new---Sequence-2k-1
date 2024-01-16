c4189924b425a3613dc8e2e3302ed093eb310380
Miroslav Tzarev <mtzarev@gmail.com>
2024-01-16 16:58:19 +0200

    Add files via upload

---
+++ newSequence 2k+1.js
@@ -0,0 +1,11 @@
+function newSequence (input){
+let num = Number(input[0]);
+let curentNum=1
+while (curentNum<=num){
+console.log(curentNum)
+curentNum = curentNum*2+1
+}
+
+    
+}
+newSequence(["10"])
